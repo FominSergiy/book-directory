77d63589fe2a7697aa85d76a79bb17e64c74f482
see-hoo <see-hoo@gmail.com>
2023-06-30 10:56:10 -0400

    Add logging middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { PrismaModule } from './modules/prisma/prisma.module';
 import { BookModule } from './modules/book/book.module';
+import { LoggerMiddleware } from './logger.middleware';
 
 @Module({
   imports: [PrismaModule, BookModule],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes('*')
+  }
+}
